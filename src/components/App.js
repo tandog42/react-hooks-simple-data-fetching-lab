1a87e6483eccdf147778af7f97f4602720271616
Tanner <troxs690@gmail.com>
2022-06-30 16:02:11 -0500

    DONE

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setimage] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(r => r.json())
+      .then(dog => {
+        setimage(dog.message);
+      });
+  }, []);
+  if (!image) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={image} alt="A Random Doggo" />
+    </div>
+  );
+}
+
+export default App;
